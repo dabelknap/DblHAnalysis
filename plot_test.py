3e056dd84edd03b18fb602cfad431feaf1ce36e2
Austin Belknap <dabelknap@wisc.edu>
2014-02-17 17:56:05 +0100

    Add 4e and graph/axis labels

--- plot_test.py
+++ plot_test.py
@@ -4,7 +4,9 @@ import matplotlib.pyplot as plt
 import scipy.stats as sp_stat
 from itertools import chain
 
-madgraph = True
+madgraph = False
+
+chan = 'mmmm'
 
 if madgraph:
     NEVENTS = 4807893.0 # ZZJets
@@ -13,14 +15,24 @@ if madgraph:
 else:
     NEVENTS = 1499064.0 # ZZTo4Mu
     XSEC = 0.07691 # ZZTo4Mu
-    MC_NAME = 'ntuples/ZZTo4mu_8TeV-powheg-pythia6.h5'
+    if chan == 'mmmm':
+        MC_NAME = 'ntuples/ZZTo4mu_8TeV-powheg-pythia6.h5'
+    if chan == 'eeee':
+        MC_NAME = 'ntuples/ZZTo4e_8TeV-powheg-pythia6.h5'
 
-#LUMI = 19.6e3
-#LUMI_A = 854.716 # DoubleMuParked 2012A
-LUMI_A = 886.75482
-LUMI_B = 3828.3217
-LUMI_C = 6375.8242
-LUMI_D = 6612.6485
+if chan == 'mmmm':
+    data_name = 'DoubleMu'
+    LUMI_A = 886.75482
+    LUMI_B = 3828.3217
+    LUMI_C = 6375.8242
+    LUMI_D = 6612.6485
+elif chan == 'eeee':
+    data_name = 'DoubleElectron'
+    LUMI_A = 886.75482
+    LUMI_B = 3828.3217
+    LUMI_C = 6375.8242
+    LUMI_D = 6612.6485
+
 LUMIS = {'A': LUMI_A,
          'B': LUMI_B,
          'C': LUMI_C,
@@ -35,13 +47,13 @@ mass0 = []
 z1mass0 = []
 z2mass0 = []
 for label in datasets:
-    if label == 'A':
-        filename = 'data_DoubleMu_Run2012A_22Jan2013_v1.h5'
-    else:
+    if label != 'A' and chan == 'mmmm':
         filename = 'data_DoubleMuParked_Run2012%s_22Jan2013_v1.h5' % label
+    else:
+        filename = 'data_%s_Run2012A_22Jan2013_v1.h5' % data_name
 
     with tb.open_file('ntuples/' + filename, 'r') as h5file:
-        table   = h5file.root.ZZ4l.mmmm
+        table   = getattr(h5file.root.ZZ4l, chan)
         mass0.extend([x['mass'] for x in table.where(CUT)])
         z1mass0.extend([x['z1mass'] for x in table.where(CUT)])
         z2mass0.extend([x['z2mass'] for x in table.where(CUT)])
@@ -50,12 +62,14 @@ print "Obs:", len(mass0)
 
 
 with tb.open_file(MC_NAME, 'r') as h5file:
-    table = h5file.root.ZZ4l.mmmm
+    table = getattr(h5file.root.ZZ4l, chan)
     mass   = np.fromiter((x['mass'] for x in table.where(CUT)),np.float)
     z1mass = np.fromiter((x['z1mass'] for x in table.where(CUT)),np.float)
     z2mass = np.fromiter((x['z2mass'] for x in table.where(CUT)),np.float)
-    weights = np.ones(mass.shape) * sum((LUMIS[x] for x in datasets))  * XSEC / NEVENTS
+    #weights = np.ones(mass.shape) * sum((LUMIS[x] for x in datasets))  * XSEC / NEVENTS
+    weights = np.ones(mass.shape) * 19.6e3  * XSEC / NEVENTS
 
+print "Lumi: %.2f ifb" % (sum((LUMIS[x] for x in datasets)) / 1000.0)
 print "Exp: %.2f +/- %.2f" % (sum(weights), np.sqrt(sum(weights)))
 print "P-val: %.4f sig" % sp_stat.norm.isf(sp_stat.poisson.sf(len(mass0), sum(weights)))
 
@@ -74,18 +88,30 @@ zzstyle = {'facecolor': 'lightskyblue',
 plt.hist(mass, bins=bins, weights=weights, histtype='stepfilled', **zzstyle)
 plt.errorbar(0.5*(bins[1:]+bins[:-1]), n, yerr=np.sqrt(n), **errbar_settings)
 plt.legend(loc='best')
-plt.savefig('./plots/test_mass.pdf')
+plt.ylim(ymin=0)
+plt.title('%s, 8 TeV, 19.6 ifb' % chan)
+plt.xlabel('4l Mass [GeV]')
+plt.ylabel('Events/%.1f GeV' % (bins[1] - bins[0]))
+plt.savefig('./plots/%s_mass.pdf' % chan)
 
 plt.clf()
 (n, bins) = np.histogram(z1mass0, 25, range=(60,120))
 plt.hist(z1mass, bins=bins, weights=weights, histtype='stepfilled', **zzstyle)
 plt.errorbar(0.5*(bins[1:]+bins[:-1]), n, yerr=np.sqrt(n), **errbar_settings)
 plt.legend(loc='best')
-plt.savefig('./plots/test_z1mass.pdf')
+plt.ylim(ymin=0)
+plt.title('%s, 8 TeV, 19.6 ifb' % chan)
+plt.xlabel('Z1 Mass [GeV]')
+plt.ylabel('Events/%.1f GeV' % (bins[1] - bins[0]))
+plt.savefig('./plots/%s_z1mass.pdf' % chan)
 
 plt.clf()
 (n, bins) = np.histogram(z2mass0, 25, range=(40,120))
 plt.hist(z2mass, bins=bins, weights=weights, histtype='stepfilled', **zzstyle)
 plt.errorbar(0.5*(bins[1:]+bins[:-1]), n, yerr=np.sqrt(n), **errbar_settings)
 plt.legend(loc='best')
-plt.savefig('./plots/test_z2mass.pdf')
+plt.ylim(ymin=0)
+plt.title('%s, 8 TeV, 19.6 ifb' % chan)
+plt.xlabel('Z2 Mass [GeV]')
+plt.ylabel('Events/%.1f GeV' % (bins[1] - bins[0]))
+plt.savefig('./plots/%s_z2mass.pdf' % chan)
