4f5188abc0a36bea26658a622ade468611fa3e69
Austin Belknap <dabelknap@wisc.edu>
2014-03-10 16:35:49 +0100

    PEP8

--- Plotter.py
+++ Plotter.py
@@ -25,10 +25,10 @@ mpl.rcParams.update(pgf_with_pdflatex)
 import matplotlib.pyplot as plt
 
 
-
 class Plotter(object):
 
-    def __init__(self, analysis, base_selections, ntuple_dir, out_dir, channels=[], lumi=19.7):
+    def __init__(self, analysis, base_selections, ntuple_dir, out_dir,
+            channels=[], lumi=19.7):
         self.base_selections = base_selections
         self.analysis = analysis
         self.out_dir = out_dir
@@ -39,7 +39,6 @@ class Plotter(object):
         self.lumi = lumi
 
         os.system("mkdir -p %s" % self.out_dir)
-
 
     def add_group(self, group_name, label, *sample_names, **kwargs):
         """
@@ -53,11 +52,11 @@ class Plotter(object):
         facecolor = kwargs.get('facecolor', 'white')
 
         self.sample_groups[group_name] = {
-                'sample_names': [os.path.splitext(os.path.basename(x))[0] for x in samples],
+                'sample_names': [os.path.splitext(os.path.basename(x))[0]
+                                 for x in samples],
                 'edgecolor': edgecolor,
                 'facecolor': facecolor,
                 'label': label}
-
 
     def stack_order(self, *sample_order):
         """
@@ -68,7 +67,6 @@ class Plotter(object):
             if sample not in self.sample_groups:
                 raise ValueError("%s not defined" % sample)
             self.sample_order.append(sample)
-
 
     def plot_stack(self, file_name, var, nbins, xmin, xmax, **kwargs):
         """
@@ -96,18 +94,21 @@ class Plotter(object):
             wgts = []
 
             for sample_name in self.sample_groups[mc]["sample_names"]:
-                with tb.open_file("%s/%s.h5" % (self.ntuple_dir, sample_name), 'r') as h5file:
+                with tb.open_file("%s/%s.h5" % (self.ntuple_dir, sample_name),
+                        'r') as h5file:
                     for chan in self.channels:
-                        table = getattr(getattr(h5file.root, self.analysis), chan)
+                        table = getattr(getattr(h5file.root, self.analysis),
+                                        chan)
                         vals += [x[var] for x in table.where(cut)]
-                        scale = self.lumi * xsec.xsecs[sample_name] / xsec.nevents[sample_name]
+                        scale = self.lumi * xsec.xsecs[sample_name] / \
+                                xsec.nevents[sample_name]
                         wgts += [x['weight'] * scale for x in table.where(cut)]
 
             values.append(vals)
             weights.append(wgts)
             labels += [self.sample_groups[mc]['label']]
 
-        plt.figure(figsize=(5,4))
+        plt.figure(figsize=(5, 4))
         (n, bins, patches) = plt.hist(
                 values, nbins, weights=weights, range=(xmin, xmax),
                 label=labels, **hist_style)
@@ -120,7 +121,7 @@ class Plotter(object):
         plt.legend(loc=legend_loc)
         plt.ylim(ymin=0)
         if ylab_width:
-            plt.ylabel('Events/%.1f GeV' % (bins[1]-bins[0]))
+            plt.ylabel('Events/%.1f GeV' % (bins[1] - bins[0]))
         else:
             plt.ylabel(ylab)
         plt.xlabel(xlab)
@@ -131,12 +132,14 @@ class Plotter(object):
         plt.savefig("%s/%s" % (self.out_dir, file_name))
 
 
-
 def main():
-    plotter = Plotter("DblH4l", "(mass > 80)", "./ntuples", "./plots/test", channels=["mmmm"], lumi=19.7)
+    plotter = Plotter("DblH4l", "(mass > 80)", "./ntuples", "./plots/test",
+                      channels=["mmmm"], lumi=19.7)
 
-    plotter.add_group("hpp", "HPP", "HPlus*110*", facecolor='mediumorchid', edgecolor='indigo')
-    plotter.add_group("zz", "ZZ", "ZZTo*", facecolor='lightskyblue', edgecolor='darkblue')
+    plotter.add_group("hpp", "HPP", "HPlus*110*",
+                      facecolor='mediumorchid', edgecolor='indigo')
+    plotter.add_group("zz", "ZZ", "ZZTo*",
+                      facecolor='lightskyblue', edgecolor='darkblue')
 
     plotter.stack_order("zz", "hpp")
 
