90ec86eca05fc46b211735f77026bf48effc51f5
Austin Belknap <dabelknap@wisc.edu>
2014-03-10 16:21:37 +0100

    Add basic plotter class and xsec library

---
+++ Plotter.py
@@ -0,0 +1,138 @@
+import sys
+import os
+import glob
+
+import numpy as np
+import tables as tb
+import xsec
+
+import matplotlib as mpl
+
+mpl.use("pgf")
+pgf_with_pdflatex = {
+        "pgf.texsystem": "pdflatex",
+        "pgf.preamble": [
+            r"\usepackage[utf8x]{inputenc}",
+            r"\usepackage[T1]{fontenc}",
+            r"\usepackage{libertine}",
+            r"\usepackage[libertine,cmintegrals,cmbraces]{newtxmath}"
+            ]
+        }
+mpl.rcParams.update(pgf_with_pdflatex)
+
+import matplotlib.pyplot as plt
+
+
+
+class Plotter(object):
+
+    def __init__(self, analysis, base_selections, ntuple_dir, out_dir, channels=[], lumi=19.7):
+        self.base_selections = base_selections
+        self.analysis = analysis
+        self.out_dir = out_dir
+        self.ntuple_dir = ntuple_dir
+        self.sample_groups = {}
+        self.sample_order = []
+        self.channels = channels
+        self.lumi = lumi
+
+        os.system("mkdir -p %s" % self.out_dir)
+
+
+    def add_group(self, group_name, label, *sample_names, **kwargs):
+        samples = []
+        for name in sample_names:
+            samples += glob.glob("%s/%s" % (self.ntuple_dir, name))
+
+        edgecolor = kwargs.get('edgecolor', 'black')
+        facecolor = kwargs.get('facecolor', 'white')
+
+        self.sample_groups[group_name] = {
+                'sample_names': [os.path.splitext(os.path.basename(x))[0] for x in samples],
+                'edgecolor': edgecolor,
+                'facecolor': facecolor,
+                'label': label}
+
+
+    def stack_order(self, *sample_order):
+        for sample in sample_order:
+            if sample not in self.sample_groups:
+                raise ValueError("%s not defined" % sample)
+            self.sample_order.append(sample)
+
+
+    def plot_stack(self, file_name, var, nbins, xmin, xmax, **kwargs):
+        values = []
+        weights = []
+        labels = []
+
+        cuts = kwargs.get('cuts', '(True)')
+        legend_loc = kwargs.get('legend_loc', 'best')
+        ylab_width = kwargs.get('label_bin_width', False)
+        ylab = kwargs.get('ylab', '')
+        xlab = kwargs.get('xlab', '')
+        title = kwargs.get('title', '')
+
+        hist_style = {'histtype': 'stepfilled',
+                      'linewidth': 1.5,
+                      'stacked': True}
+
+        cut = "%s & %s" % (self.base_selections, cuts)
+
+        for mc in self.sample_order:
+            vals = []
+            wgts = []
+
+            for sample_name in self.sample_groups[mc]["sample_names"]:
+                with tb.open_file("%s/%s.h5" % (self.ntuple_dir, sample_name), 'r') as h5file:
+                    for chan in self.channels:
+                        table = getattr(getattr(h5file.root, self.analysis), chan)
+                        vals += [x[var] for x in table.where(cut)]
+                        scale = self.lumi * xsec.xsecs[sample_name] / xsec.nevents[sample_name]
+                        wgts += [x['weight'] * scale for x in table.where(cut)]
+
+            values.append(vals)
+            weights.append(wgts)
+            labels += [self.sample_groups[mc]['label']]
+
+        plt.figure(figsize=(5,4))
+        (n, bins, patches) = plt.hist(
+                values, nbins, weights=weights, range=(xmin, xmax),
+                label=labels, **hist_style)
+
+        for i, name in enumerate(self.sample_order):
+            for p in patches[i]:
+                p.set_facecolor(self.sample_groups[name]['facecolor'])
+                p.set_edgecolor(self.sample_groups[name]['edgecolor'])
+
+        plt.legend(loc=legend_loc)
+        plt.ylim(ymin=0)
+        if ylab_width:
+            plt.ylabel('Events/%.1f GeV' % (bins[1]-bins[0]))
+        else:
+            plt.ylabel(ylab)
+        plt.xlabel(xlab)
+        plt.title(title)
+
+        plt.tight_layout(0.5)
+
+        plt.savefig("%s/%s" % (self.out_dir, file_name))
+
+
+
+def main():
+    plotter = Plotter("DblH4l", "(mass > 80)", "./ntuples", "./plots/test", channels=["mmmm"], lumi=19.7)
+
+    plotter.add_group("hpp", "HPP", "HPlus*110*", facecolor='mediumorchid', edgecolor='indigo')
+    plotter.add_group("zz", "ZZ", "ZZTo*", facecolor='lightskyblue', edgecolor='darkblue')
+
+    plotter.stack_order("zz", "hpp")
+
+    plotter.plot_stack('test.pdf', 'h1mass', 10, 10, 210,
+            title=r'$\sqrt{s}=$ 8 TeV, 19.7 $\text{fb}^{-1}$',
+            xlab=r'$M_{l^+l^+}$ [GeV]',
+            label_bin_width=True)
+
+
+if __name__ == "__main__":
+    main()
---
+++ xsec.py
@@ -0,0 +1,36 @@
+PB = 1.0e3
+FB = 1.0
+
+xsecs = {'HPlusPlusHMinusMinusHTo4L_M-110_8TeV-pythia6': 352.49 * FB,
+         'HPlusPlusHMinusMinusHTo4L_M-400_8TeV-pythia6': 1.3414 * FB,
+         'HPlusPlusHMinusMinusHTo4L_M-500_8TeV-pythia6': 0.39604 * FB,
+
+         'DYJetsToLL_M-10To50filter_8TeV-madgraph': 915.0 * PB,
+         'DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball': 3503.71 * PB,
+
+         'ZZTo4e_8TeV-powheg-pythia6': 0.07691 * PB,
+         'ZZTo4mu_8TeV-powheg-pythia6': 0.07691 * PB,
+
+         'T_s-channel_TuneZ2star_8TeV-powheg-tauola': 3.79 * PB,
+         'T_t-channel_TuneZ2star_8TeV-powheg-tauola': 56.4 * PB,
+         'T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 11.1 * PB,
+         'Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola': 1.56 * PB,
+         'Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola': 30.7 * PB,
+         'Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 11.1 * PB}
+
+nevents = {'HPlusPlusHMinusMinusHTo4L_M-110_8TeV-pythia6': 1249577.0,
+           'HPlusPlusHMinusMinusHTo4L_M-400_8TeV-pythia6': 250000.0,
+           'HPlusPlusHMinusMinusHTo4L_M-500_8TeV-pythia6': 125000.0,
+
+           'DYJetsToLL_M-10To50filter_8TeV-madgraph': 7095797.0,
+           'DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball': 30056073.0,
+
+           'ZZTo4e_8TeV-powheg-pythia6': 1476608.0,
+           'ZZTo4mu_8TeV-powheg-pythia6': 1499064.0,
+
+           'T_s-channel_TuneZ2star_8TeV-powheg-tauola': 259961.0,
+           'T_t-channel_TuneZ2star_8TeV-powheg-tauola': 96351.0,
+           'T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 497658.0,
+           'Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola': 139974.0,
+           'Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola': 1879558.0,
+           'Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 493460.0}
