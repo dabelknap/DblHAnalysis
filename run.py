6623ecae172132a98c080ed88fa08b8d727b9337
Austin Belknap <dabelknap@wisc.edu>
2014-04-11 14:06:50 +0200

    Remove old 4l analyzer

--- run.py
+++ run.py
@@ -1,4 +1,3 @@
-from analyzers import analyze4l as an4l
 from analyzers import analyzeZZ as anZZ
 from analyzers import Analyzer4l
 from analyzers import Control4l
@@ -13,17 +12,6 @@ def main():
     sample_names = [os.path.basename(fname)
                     for string in sys.argv[2:]
                     for fname in glob.glob("%s/%s" % (root_dir, string))]
-
-    if sys.argv[1] == "4l_old":
-        for name in sample_names:
-            print "processing %s" % name
-            with an4l.Analyzer4lMMMM("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as muanalyzer:
-                print "4mu analyzer"
-                muanalyzer.analyze()
-
-            with an4l.Analyzer4lEEEE("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as eleanalyzer:
-                print "4ele analyzer"
-                eleanalyzer.analyze()
 
     elif sys.argv[1] == "4l":
         for name in sample_names:
@@ -47,13 +35,6 @@ def main():
             with anZZ.ZZAnalyzerEEEE("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as eleanalyzer:
                 print "4ele analyzer"
                 eleanalyzer.analyze()
-
-    elif sys.argv[1] == "ctrl_old":
-        for name in sample_names:
-            print "processing %s" % name
-            with an4l.AnalyzerControlMMMM("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as muanalyzer:
-                print "4mu control analyzer"
-                muanalyzer.analyze()
 
     else:
         raise ValueError("%s is invalid option" % sys.argv[1])
