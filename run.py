08f9b59b1040ad66b502c31572c6ac0638a8c64d
Austin Belknap <dabelknap@wisc.edu>
2014-04-14 13:44:27 +0200

    Use a more pythonic structure for run.py

--- run.py
+++ run.py
@@ -1,46 +1,85 @@
+#!/usr/bin/env python
+"""
+This is a run script for running the H++/H-- analysis on FSA n-tuples.
+
+Author: D. Austin Belknap, UW-Madison
+"""
+
+import os
+import sys
+import glob
+import argparse
+
 from analyzers import analyzeZZ as anZZ
 from analyzers import Analyzer4l
 from analyzers import Control4l
-import glob
-import os
-import sys
 
-def main():
+
+def run_ntuples(analyzer_type, samples):
     root_dir = './root_files'
     ntup_dir = './ntuples'
 
     sample_names = [os.path.basename(fname)
-                    for string in sys.argv[2:]
+                    for string in samples
                     for fname in glob.glob("%s/%s" % (root_dir, string))]
 
-    elif sys.argv[1] == "4l":
+    if analyzer_type == "4l":
         for name in sample_names:
             print "Processing %s" % name
-            with Analyzer4l("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as analyzer:
+            with Analyzer4l("%s/%s" % (root_dir, name),
+                            "%s/%s.h5" % (ntup_dir, name)) as analyzer:
                 analyzer.analyze()
 
-    elif sys.argv[1] == "ctrl":
+    elif analyzer_type == "ctrl":
         for name in sample_names:
             print "Processing %s" % name
-            with Control4l("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as analyzer:
+            with Control4l("%s/%s" % (root_dir, name),
+                           "%s/%s.h5" % (ntup_dir, name)) as analyzer:
                 analyzer.analyze()
 
-    elif sys.argv[1] == "zz":
+    elif analyzer_type == "zz":
         for name in sample_names:
             print "processing %s" % name
-            with anZZ.ZZAnalyzerMMMM("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as muanalyzer:
+            with anZZ.ZZAnalyzerMMMM("%s/%s" % (root_dir, name),
+                                     "%s/%s.h5" % (ntup_dir, name)) as mAnlyzr:
                 print "4mu analyzer"
-                muanalyzer.analyze()
+                mAnlyzer.analyze()
 
-            with anZZ.ZZAnalyzerEEEE("%s/%s" % (root_dir, name), "%s/%s.h5" % (ntup_dir, name)) as eleanalyzer:
+            with anZZ.ZZAnalyzerEEEE("%s/%s" % (root_dir, name),
+                                     "%s/%s.h5" % (ntup_dir, name)) as eAnlyzr:
                 print "4ele analyzer"
-                eleanalyzer.analyze()
+                eAnlyzer.analyze()
 
     else:
-        raise ValueError("%s is invalid option" % sys.argv[1])
+        raise ValueError("%s is invalid option" % analyzer_type)
+
+    return 0
+
+
+
+def parse_command_line(argv):
+    parser = argparse.ArgumentParser(description="Run the desired analyzer on "
+                                                 "FSA n-tuples")
+
+    parser.add_argument('analyzer', type=str, help='4l, ctrl, or zz')
+    parser.add_argument('sample_names', nargs='+',
+                        help='Sample names w/ UNIX wildcards')
+    args = parser.parse_args(argv)
+
+    return args
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv[1:]
+
+    args = parse_command_line(argv)
+
+    run_ntuples(args.analyzer, args.sample_names)
 
     return 0
 
 
 if __name__ == "__main__":
-    main()
+    status = main()
+    sys.exit(status)
