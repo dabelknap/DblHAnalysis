0a151b1875616b7c344d336a5432281b4715705c
Austin Belknap <dabelknap@wisc.edu>
2014-05-11 16:49:52 +0200

    Use nevents from json file

--- plotters/xsec.py
+++ plotters/xsec.py
@@ -1,6 +1,8 @@
 PB = 1.0e3
 FB = 1.0
 
+import json
+
 xsecs = {'HPlusPlusHMinusMinusHTo4L_M-110_8TeV-pythia6': 352.49 * FB,
          'HPlusPlusHMinusMinusHTo4L_M-400_8TeV-pythia6': 1.3414 * FB,
          'HPlusPlusHMinusMinusHTo4L_M-450_8TeV-pythia6': 0.71531 * FB,
@@ -9,8 +11,17 @@ xsecs = {'HPlusPlusHMinusMinusHTo4L_M-110_8TeV-pythia6': 352.49 * FB,
          'DYJetsToLL_M-10To50filter_8TeV-madgraph': 915.0 * PB,
          'DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball': 3503.71 * PB,
 
+         'Z1jets_M50': 100000.0,
+         'Z2jets_M50_S10': 100000.0,
+         'Z3jets_M50': 100000.0,
+         'Z4jets_M50': 100000.0,
+
          'ZZTo4e_8TeV-powheg-pythia6': 0.07691 * PB,
          'ZZTo4mu_8TeV-powheg-pythia6': 0.07691 * PB,
+         'ZZTo4tau_8TeV-powheg-pythia6': 0.07691 * PB,
+         'ZZTo2e2mu_8TeV-powheg-pythia6': 0.1767 * PB,
+         'ZZTo2e2tau_8TeV-powheg-pythia6': 0.1767 * PB,
+         'ZZTo2mu2tau_8TeV-powheg-pythia6': 0.1767 * PB,
 
          'ZZJetsTo4L_TuneZ2star_8TeV-madgraph-tauola': 0.1296 * PB,
 
@@ -21,22 +32,4 @@ xsecs = {'HPlusPlusHMinusMinusHTo4L_M-110_8TeV-pythia6': 352.49 * FB,
          'Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola': 30.7 * PB,
          'Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 11.1 * PB}
 
-nevents = {'HPlusPlusHMinusMinusHTo4L_M-110_8TeV-pythia6': 1249577.0,
-           'HPlusPlusHMinusMinusHTo4L_M-400_8TeV-pythia6': 250000.0,
-           'HPlusPlusHMinusMinusHTo4L_M-450_8TeV-pythia6': 248310.0,
-           'HPlusPlusHMinusMinusHTo4L_M-500_8TeV-pythia6': 125000.0,
-
-           'DYJetsToLL_M-10To50filter_8TeV-madgraph': 7095797.0,
-           'DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball': 30056073.0,
-
-           'ZZTo4e_8TeV-powheg-pythia6': 1476608.0,
-           'ZZTo4mu_8TeV-powheg-pythia6': 1499064.0,
-
-           'ZZJetsTo4L_TuneZ2star_8TeV-madgraph-tauola': 4807893.0,
-
-           'T_s-channel_TuneZ2star_8TeV-powheg-tauola': 259961.0,
-           'T_t-channel_TuneZ2star_8TeV-powheg-tauola': 96351.0,
-           'T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 497658.0,
-           'Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola': 139974.0,
-           'Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola': 1879558.0,
-           'Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola': 493460.0}
+nevents = json.load(open("./plotters/mc_events.json", 'r'))
