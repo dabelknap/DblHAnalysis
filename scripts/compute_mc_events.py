0a6f4b3ea276f4b73307e7d87112520efd94c7e5
Austin Belknap <dabelknap@wisc.edu>
2014-05-11 16:49:15 +0200

    Add script to extract MC event counts

---
+++ scripts/compute_mc_events.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import argparse
+import json
+import glob
+import logging
+
+sys.argv.append('-b')
+import ROOT as rt
+sys.argv.pop()
+
+
+def count_events(rtfile_name):
+    rtfile = rt.TFile(rtfile_name, "READ")
+
+    tree = rtfile.Get("mmmm/metaInfo")
+
+    if not tree:
+        raise SystemExit(1)
+
+    total_events = 0
+
+    for entry in xrange(tree.GetEntries()):
+        tree.GetEntry(entry)
+        total_events += tree.nevents
+
+    rtfile.Close()
+
+    return total_events
+
+
+def sample_counts(sample_location):
+    filenames = os.listdir(sample_location)
+
+    counts = 0
+    for f in filenames:
+        counts += count_events("%s/%s" % (sample_location, f))
+
+    return counts
+
+
+def main(argv=None):
+    log = logging.getLogger(__name__)
+
+    logging.basicConfig(level=logging.INFO)
+
+    root_dir = "./root_files"
+
+    sample_names = [os.path.basename(fname)
+                    for string in sys.argv[1:]
+                    for fname in glob.glob("%s/%s" % (root_dir, string))]
+
+    out = {}
+    for name in sample_names:
+        out[name] = sample_counts("%s/%s" % (root_dir, name))
+        log.info("Processing %s: %i" % (name, out[name]))
+
+    outfile = open('./plotters/mc_events.json', 'w')
+    outfile.write(json.dumps(out, indent=4, sort_keys=True))
+
+    outfile.close()
+
+
+if __name__ == "__main__":
+    main()
