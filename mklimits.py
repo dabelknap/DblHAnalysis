c39ff8790e1f4c17cf583a67a7947252d7845c95
Austin Belknap <dabelknap@wisc.edu>
2014-09-09 14:04:09 -0500

    Add script to run limits

---
+++ mklimits.py
@@ -0,0 +1,28 @@
+from limits import Limits
+import sys
+
+def four_l():
+    limits = Limits("DblH", "(mass > 0)", "./ntuples", "./datacards/4l",
+            channels=["dblh4l"], lumi=19.7, blinded=True)
+
+    limits.add_group("hpp450", "HPlus*450*", isSignal=True)
+    limits.add_group("dyjets", "DYJets*")
+    limits.add_group("zz", "ZZTo*")
+    limits.add_group("top", "T*")
+
+    lumi = {'hpp450': 0.026,
+            'dyjets': 0.026,
+            'zz':     0.026,
+            'top':    0.026}
+
+    limits.add_systematics("lumi", "lnN", **lumi)
+
+    limits.gen_card("test.txt")
+
+
+def main():
+     four_l()
+
+
+if __name__ == "__main__":
+    main()
