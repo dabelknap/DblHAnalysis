83f063eab73868fa7e2d19ae482eacb174c487d7
Austin Belknap <dabelknap@wisc.edu>
2014-09-10 10:22:44 -0500

    Run multiple mass points

--- mklimits.py
+++ mklimits.py
@@ -1,11 +1,20 @@
 from limits import Limits
+import logging
 import sys
 
-def four_l():
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+_4L_MASSES = [110, 130, 150, 170, 200, 250, 300,
+              350, 400, 450, 500, 600, 700]
+
+def four_l(mass):
+    logger.info("Process mass-point %i" % mass)
+
     limits = Limits("DblH", "(mass > 0)", "./ntuples", "./datacards/4l",
             channels=["dblh4l"], lumi=19.7, blinded=True)
 
-    limits.add_group("hpp450", "HPlus*450*", isSignal=True)
+    limits.add_group("hpp%i" % mass, "HPlus*%i*" % mass, isSignal=True)
     limits.add_group("dyjets", "DYJets*")
     limits.add_group("zz", "ZZTo*")
     limits.add_group("top", "T*")
@@ -18,11 +27,13 @@ def four_l():
 
     limits.add_systematics("lumi", "lnN", **lumi)
 
-    limits.gen_card("test.txt")
+    limits.gen_card("%i.txt" % mass)
 
 
 def main():
-     four_l()
+
+    for mass in _4L_MASSES:
+        four_l(mass)
 
 
 if __name__ == "__main__":
