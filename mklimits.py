e8f7a8d2c17546db1951ee24c22fd57788ea83dd
Austin Belknap <dabelknap@wisc.edu>
2014-09-10 10:48:33 -0500

    Add mass/sT cuts

--- mklimits.py
+++ mklimits.py
@@ -9,9 +9,13 @@ _4L_MASSES = [110, 130, 150, 170, 200, 250, 300,
               350, 400, 450, 500, 600, 700]
 
 def four_l(mass):
-    logger.info("Process mass-point %i" % mass)
+    logger.info("Processing mass-point %i" % mass)
 
-    limits = Limits("DblH", "(mass > 0)", "./ntuples", "./datacards/4l",
+    cuts = '(%f < h1mass) & (h1mass < %f)' % (0.9*mass, 1.1*mass)
+    cuts += '& (%f < sT)' % (0.6*mass + 130.0)
+    cuts += '& (channel == "mmmm")'
+
+    limits = Limits("DblH", cuts, "./ntuples", "./datacards/4l",
             channels=["dblh4l"], lumi=19.7, blinded=True)
 
     limits.add_group("hpp%i" % mass, "HPlus*%i*" % mass, isSignal=True)
@@ -20,12 +24,17 @@ def four_l(mass):
     limits.add_group("top", "T*")
     limits.add_group("data", "data_*", isData=True)
 
-    lumi = {'hpp450': 0.026,
-            'dyjets': 0.026,
-            'zz':     0.026,
-            'top':    0.026}
-
+    lumi = {'hpp%i' % mass: 0.026,
+            'dyjets':       0.026,
+            'zz':           0.026,
+            'top':          0.026}
     limits.add_systematics("lumi", "lnN", **lumi)
+
+    mu_eff = {'hpp%i' % mass: 0.043,
+              'dyjets':       0.043,
+              'zz':           0.043,
+              'top':          0.043}
+    limits.add_systematics("mu_eff", "lnN", **mu_eff)
 
     limits.gen_card("%i.txt" % mass)
 
