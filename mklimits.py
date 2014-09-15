21af884ca4ef142aff15f83675da0e377cc0417e
Austin Belknap <dabelknap@wisc.edu>
2014-09-15 13:51:46 -0500

    Add emem and meme

--- mklimits.py
+++ mklimits.py
@@ -40,18 +40,58 @@ def mmmm_100(mass):
     limits.gen_card("mmmm.txt")
 
 
+def fourl(mass):
+    logger.info("Processing mass-point %i" % mass)
+
+    cuts = '(%f < h1mass) & (h1mass < %f)' % (0.9*mass, 1.1*mass)
+    cuts += '& (%f < sT)' % (0.6*mass + 130.0)
+    cuts += ('& ((channel == "mmmm") | (channel == "eeee") | (channel == "eemm") |'
+             '(channel == "mmee") | (channel == "meme") | (channel == "emem"))')
+
+    limits = Limits("DblH", cuts, "./ntuples", "./datacards/fourl/%i" % mass,
+            channels=["dblh4l"], lumi=19.7, blinded=True)
+
+    limits.add_group("hpp%i" % mass, "HPlus*%i*" % mass, isSignal=True)
+    limits.add_group("dyjets", "DYJets*")
+    limits.add_group("zz", "ZZTo*")
+    limits.add_group("top", "T*")
+    limits.add_group("data", "data_*", isData=True)
+
+    lumi = {'hpp%i' % mass: 1.026,
+            'dyjets':       1.026,
+            'zz':           1.026,
+            'top':          1.026}
+    limits.add_systematics("lumi", "lnN", **lumi)
+
+    mu_eff = {'hpp%i' % mass: 1.043,
+              'dyjets':       1.043,
+              'zz':           1.043,
+              'top':          1.043}
+    limits.add_systematics("mu_eff", "lnN", **mu_eff)
+
+    limits.gen_card("4l.txt")
+
+
 def plot_mmmm():
     plot_limits("test.pdf", _4L_MASSES, "datacards/4l", blinded=True,
                 x_label=r"$\Phi^{++}$ Mass [GeV]",
                 y_label=r"95% CL Upper Limit on $\sigma/\sigma_{SM}$")
 
 
+def plot_4l():
+    plot_limits("test_4l.pdf", _4L_MASSES, "datacards/fourl", blinded=True,
+                x_label=r"$\Phi^{++}$ Mass [GeV]",
+                y_label=r"95% CLs Upper Limit on $\sigma/\sigma_{SM}$")
+
+
+
 def main():
 
-    plot_mmmm()
+    #plot_mmmm()
+    plot_4l()
 
     #for mass in _4L_MASSES:
-    #    mmmm_100(mass)
+    #    fourl(mass)
 
 
 if __name__ == "__main__":
