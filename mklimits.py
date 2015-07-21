e1fd87503f749f1a4b581ca7f609fbd0e5d4af4d
Austin Belknap <dabelknap@wisc.edu>
2015-07-21 15:24:45 -0500

    Use BP name for systematic names

--- mklimits.py
+++ mklimits.py
@@ -101,10 +101,10 @@ def four_lepton(name, channels, directory, scale=1.0):
             '(%s)' % ' | '.join(['(channel == "%s")' % channel for channel in channels]))
 
         limits.add_bkg_rate("bkg_sb_%s" % channels[0], float(N_db_data) * alpha)
-        kwargs = {"bkg_sb_%s" % channels[0]: alpha}
+        kwargs = {"bkg_sb_%s" % name: alpha}
         limits.add_systematics("bkg_err_%s" % channels[0], "gmN %i" % N_db_data, **kwargs)
 
-        kwargs = {"bkg_sb_%s" % channels[0]: 1.10}
+        kwargs = {"bkg_sb_%s" % name: 1.10}
         limits.add_systematics("alph_err", "lnN", **kwargs)
 
         limits.gen_card("%s.txt" % name)
